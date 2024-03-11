41e175c6760e573572061caeea643b98066126ce
PenzinMax <99172843+PenzinMax@users.noreply.github.com>
2024-03-11 08:46:07 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-﻿namespace numbers
+﻿namespace ConsoleApp1
 {
-    using System;
     internal class Program
     {
         static void Main(string[] args)
@@ -8,36 +7,37 @@
             if (args.Length != 2)
             {
                 Console.WriteLine("Пожалуйста, введите два числа через пробел");
-                
+
             }
             else
             {
-                
-                    try
-                    {
-                        int num1 = int.Parse(args[0]);
-                        int num2 = int.Parse(args[1]);
 
-                        Console.WriteLine("Сумма чисел = " + (num1 + num2));
-                        Console.WriteLine("Произведение = " + (num1 * num2));
-                        Console.WriteLine("Разница = " + (num1 - num2));
+                try
+                {
+                    int num1 = int.Parse(args[0]);
+                    int num2 = int.Parse(args[1]);
 
-                        if (num2 != 0)
-                        {
-                            Console.WriteLine("Результат деления = " + ((double)num1 / num2));
-                        }
-                        else
-                        {
-                            Console.WriteLine("Результат деления = Делить на ноль нельзя");
-                        }
-                    }
-                    catch 
+                    Console.WriteLine("Сумма чисел = " + (num1 + num2));
+                    Console.WriteLine("Произведение = " + (num1 * num2));
+                    Console.WriteLine("Разница = " + (num1 - num2));
+
+                    try 
                     {
-                        Console.WriteLine("Ошибка");
+                        Console.WriteLine("Результат деления = " + ((double)num1 / num2));
                     }
-                
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Результат деления = Делить на ноль нельзя");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                }
+
 
             }
         }
     }
+    
 }
